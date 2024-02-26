35b3a5b4b6143e0bc20a70603a33cc7d410b9ff5
tunim73 <antonio.netto973@gmail.com>
2024-02-26 10:28:06 -0300

    add create.blade

--- routes/web.php
+++ routes/web.php
@@ -14,14 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-/* Route::get('/', function () {
-    return view('welcome');
-}); */
-
-
 Route::controller(EventController::class)->group(function () {
   Route::get('/', "index");
   Route::get('/events/create', "create");
-  
 });
 
