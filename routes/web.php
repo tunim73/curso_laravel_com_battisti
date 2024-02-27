1baf8ba6de103411e103dce77c5aa3e220b63e50
tunim73 <antonio.netto973@gmail.com>
2024-02-27 07:56:30 -0300

    add middleware in the route

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::controller(EventController::class)->group(function () {
   Route::get('/', "index");
-  Route::get('/events/create', "create");
+  Route::get('/events/create', "create")->middleware('auth');
   Route::get('/events/{id}', "show");
   Route::post('/events', 'store');
 });
