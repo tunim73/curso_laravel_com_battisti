19aafe200ad831da5d8d42288249bfbe30190186
tunim73 <antonio.netto973@gmail.com>
2024-02-27 07:53:26 -0300

    add relationships

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -17,4 +17,17 @@ class Event extends Model
   protected $date = [
     'date'
   ];
+
+  public function user()
+  {
+    return $this->belongsTo(User::class);
+  }
+
+  public function users()
+  {
+    return $this->belongsToMany(User::class);
+  }
+
+
+
 }
