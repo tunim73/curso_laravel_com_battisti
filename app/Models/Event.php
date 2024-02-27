daae4448eac5df64c073c059fadfa1fb61a8a5a2
tunim73 <antonio.netto973@gmail.com>
2024-02-27 10:06:27 -0300

    add types to models

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Event extends Model
 {
@@ -18,12 +20,12 @@ class Event extends Model
     'date'
   ];
 
-  public function user()
+  public function user(): BelongsTo
   {
     return $this->belongsTo(User::class);
   }
 
-  public function users()
+  public function users(): BelongsToMany
   {
     return $this->belongsToMany(User::class);
   }
